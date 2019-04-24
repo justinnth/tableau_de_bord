f2fd2c93af69db45439a425c517754c6d267a10c
Justin North <justin_n@hotmail.fr>
2019-04-24 18:11:49 +0200

    Ajout de twig + création de la page de base pour la table des formateurs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/")
@@ -16,5 +17,13 @@ class HomeController
     public function homepage()
     {
         return new Response("Page d'accueil");
+    }
+
+    /**
+     * @Route("/formateurs")
+     */
+    public function formateurs()
+    {
+        return $this->render('formateurs/formateurs.html.twig');
     }
 }
